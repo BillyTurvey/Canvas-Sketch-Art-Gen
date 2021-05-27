db0023c0ba3746022f73a7c1123400e747212cf0
Billy Turvey <billy@billyturvey.com>
2021-05-27 20:50:31 +0100

    feat: adds grid of circles

--- sketch.js
+++ sketch.js
@@ -1,24 +1,43 @@
 import canvasSketch from 'canvas-sketch';
 
 const settings = {
-  dimensions: [ 1742, 1032 ],
-  pixelsPerInch: 300,
-  units: 'mm'
+  dimensions: [ 1048, 1048 ],
+  // pixelsPerInch: 300
 };
 
 const sketch = () => {
+
+  const createGrid = () => {
+    const points = [];
+    const count = 5;
+    for (let x = 0; x < count; x++) {
+      for (let y = 0; y < count; y++) {
+        const u = x / count;
+        const v = y / count;
+        points.push([u,v])
+      }
+    }
+    return points;
+  };
+
+  const points = createGrid();
+
+  console.log(points);
+
   return ({ context, width, height }) => {
-    console.log(width, height);
-    context.fillStyle = 'red';
+    context.fillStyle = 'orange';
     context.fillRect(0, 0, width, height);
 
-    context.beginPath();
-    context.arc(width/2, height/2, height/4, 0, Math.PI*2, false);
-    context.fillStyle = 'orange';
-    context.fill();
-    context.lineWidth = 5;
-    context.strokeStyle = 'blue';
-    context.stroke();
+    points.forEach(([u, v]) => {
+      const x = u * width; //scaling back up to width in pixels
+      const y = v * height;
+
+      context.beginPath();
+      context.arc(x + width/10, y + height/10, 100, 0, Math.PI *2, true);
+      context.strokeStyle = 'black';
+      context.lineWidth = 30;
+      context.stroke();
+    })
   };
 };
 
