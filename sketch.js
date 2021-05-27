d3c1afe7ca10740193c481976937f513da8a6af6
Billy Turvey <billy@billyturvey.com>
2021-05-27 13:45:04 +0100

    minor commit

--- sketch.js
+++ sketch.js
@@ -1,15 +1,24 @@
-import canvasSketch from 'canvas-sketch;
+import canvasSketch from 'canvas-sketch';
 
 const settings = {
-  dimensions: [ 2048, 2048 ]
+  dimensions: [ 1742, 1032 ],
+  pixelsPerInch: 300,
+  units: 'mm'
 };
 
 const sketch = () => {
   return ({ context, width, height }) => {
-    context.fillStyle = 'yellow';
+    console.log(width, height);
+    context.fillStyle = 'red';
     context.fillRect(0, 0, width, height);
+
     context.beginPath();
-    context.arc(width/2, height/2, 200, 0, Math.PI*2, false);
+    context.arc(width/2, height/2, height/4, 0, Math.PI*2, false);
+    context.fillStyle = 'orange';
+    context.fill();
+    context.lineWidth = 5;
+    context.strokeStyle = 'blue';
+    context.stroke();
   };
 };
 
